f346c58809b40f50c3f472df07bf782528e501b7
MarounC6 <marounchahine@live.com>
2025-03-07 11:56:05 +0100

    not done yet

--- Etat.cpp
+++ Etat.cpp
@@ -27,3 +27,160 @@ bool E0::transition(Automate & automate, Symbole * s) {
 }
 
 
+bool E1::transition(Automate & automate, Symbole * s) {
+    switch (*s) {
+        case PLUS:
+            automate.decalage(s, new E4);
+            break;
+        case MULT:
+            automate.decalage(s, new E5);
+            break;
+        case FIN:
+            // État d'acceptation
+            return false;
+        default:
+            automate.decalage(new Symbole(ERREUR), NULL);
+            return false;
+    }
+    return true;
+}
+
+bool E2::transition(Automate & automate, Symbole * s) {
+    switch (*s) {
+        case INT:
+            automate.decalage(s, new E3);
+            break;
+        case OPENPAR:
+            automate.decalage(s, new E2);
+            break;
+        case EXPR:
+            automate.transitionSimple(s, new E6);
+            break;
+        default:
+            automate.decalage(new Symbole(ERREUR), NULL);
+            return false;
+    }
+    return true;
+}
+
+bool E3::transition(Automate & automate, Symbole * s) {
+    Entier * s1;
+    switch (*s) {
+        case PLUS:
+        case MULT:
+        case CLOSEPAR:
+        case FIN:
+            s1 = (Entier*) automate.popSymbole();
+            automate.reduction(5, new ExprVal(s1->getValeur()));
+            break;
+        default:
+            automate.decalage(new Symbole(ERREUR), NULL);
+            return false;
+    }
+    return true;
+}
+
+bool E4::transition(Automate & automate, Symbole * s) {
+    switch (*s) {
+        case INT:
+            automate.decalage(s, new E3);
+            break;
+        case OPENPAR:
+            automate.decalage(s, new E2);
+            break;
+        default:
+            automate.decalage(new Symbole(ERREUR), NULL);
+            return false;
+    }
+    return true;
+}
+
+bool E5::transition(Automate & automate, Symbole * s) {
+    switch (*s) {
+        case INT:
+            automate.decalage(s, new E3);
+            break;
+        case OPENPAR:
+            automate.decalage(s, new E2);
+            break;
+        default:
+            automate.decalage(new Symbole(ERREUR), NULL);
+            return false;
+    }
+    return true;
+}
+
+bool E6::transition(Automate & automate, Symbole * s) {
+    switch (*s) {
+        case PLUS:
+            automate.decalage(s, new E4);
+            break;
+        case MULT:
+            automate.decalage(s, new E5);
+            break;
+        case CLOSEPAR:
+            automate.decalage(s, new E9);
+            break;
+        default:
+            automate.decalage(new Symbole(ERREUR), NULL);
+            return false;
+    }
+    return true;
+}
+
+bool E7::transition(Automate & automate, Symbole * s) {
+    switch (*s) {
+        case PLUS:
+            automate.reduction(2);
+            break;
+        case MULT:
+            automate.reduction(2);
+            break;
+        case CLOSEPAR:
+        case FIN:
+            automate.reduction(2);
+            break;
+        default:
+            automate.decalage(new Symbole(ERREUR), NULL);
+            return false;
+    }
+    return true;
+}
+
+bool E8::transition(Automate & automate, Symbole * s) {
+    switch (*s) {
+        case PLUS:
+            automate.reduction(3);
+            break;
+        case MULT:
+            automate.reduction(3);
+            break;
+        case CLOSEPAR:
+        case FIN:
+            automate.reduction(3);
+            break;
+        default:
+            automate.decalage(new Symbole(ERREUR), NULL);
+            return false;
+    }
+    return true;
+}
+
+bool E9::transition(Automate & automate, Symbole * s) {
+    switch (*s) {
+        case PLUS:
+            automate.reduction(4);
+            break;
+        case MULT:
+            automate.reduction(4);
+            break;
+        case CLOSEPAR:
+        case FIN:
+            automate.reduction(4);
+            break;
+        default:
+            automate.decalage(new Symbole(ERREUR), NULL);
+            return false;
+    }
+    return true;
+}
--- Expr.h
+++ Expr.h
@@ -16,3 +16,45 @@ class Expr: public Symbole {
         double valeur;
 }
 
+
+class ExprBin : public Expr
+{
+    public:
+        
+        ExpressionBin(Expr* gauche, Expr* droite);
+        
+        virtual ~ExprBin();
+        
+        virtual double eval()=0;
+    protected:
+        
+        Expr* gauche;
+        Expr* droite;
+};
+
+
+class ExprPlus:public ExprBin
+{
+    public:
+        ExprPlus(Expr* gauche, Expr* droite);
+        ~ExprPlus();
+        double eval();
+};
+
+class ExprMult:public ExprBin
+{
+    public:
+        ExprMult(Expr* gauche, Expr* droite);
+        ~ExprMult();
+    double eval();
+};
+
+class ExprVal:public Expr
+{
+    public:
+        ExprVal(int v);
+        ~ExprVal(){};
+        double eval();
+    private:
+        int valeur;
+};
