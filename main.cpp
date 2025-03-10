0e2099e1e1fff4870edbae4fc70c1b843ed6856e
MarounC6 <marounchahine@live.com>
2025-03-10 16:39:25 +0100

    final

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 
 int main(void) {
+
+   /*
    string chaine("(1+34)*123"); // (1+2)*4
 
    Lexer *l = new Lexer(chaine);
@@ -13,17 +15,25 @@ int main(void) {
 
    cout << "Analyse de " << chaine << endl;
    automate.run();
-
-   /*
-   Symbole * s;
-   while(*(s=l.Consulter())!=FIN) {
-      s->Affiche();
-      cout<<endl;
-      l.Avancer();
-   }
    */
 
-   //delete l;
+   string chaine;//("(1+34)*123)");
+   char on = 'y';
+   while(on=='y'){
+      cout << "Veuillez rentrer une expression arithmétique: ";
+      cin >> chaine;
+
+      Lexer *l = new Lexer(chaine);
+      Automate automate(l);
+
+      cout << chaine <<endl;
+
+      automate.run();
+      
+      cout << "Tapez 'y' pour continuer: ";
+      cin >> on;
+   }
+  
 
    return 0;
 }
