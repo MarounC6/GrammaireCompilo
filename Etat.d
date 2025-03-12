9221684649fcee7351b8569f4bc3e7e6bacee90b
MarounC6 <marounchahine@live.com>
2025-03-12 20:20:20 +0100

    adding explanations new new 3

--- Etat.d
+++
@@ -1,6 +0,0 @@
-Etat.o: Etat.cpp Etat.h symbole.h automate.h lexer.h Expr.h
-Etat.h:
-symbole.h:
-automate.h:
-lexer.h:
-Expr.h:
--- Expr.d
+++
@@ -1,3 +0,0 @@
-Expr.o: Expr.cpp Expr.h symbole.h
-Expr.h:
-symbole.h:
