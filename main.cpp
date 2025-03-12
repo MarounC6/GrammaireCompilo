207b712aa2e3ec49f08ca47c4177b6cc7f15220d
MarounC6 <marounchahine@live.com>
2025-03-12 19:45:15 +0100

    adding explanations

--- main.cpp
+++ main.cpp
@@ -20,6 +20,13 @@ int main(void) {
    cout << "Bienvenue dans le programme d'analyse syntaxique d'expressions arithmétiques" << endl;
    cout << "Nous avons décidé de considérer que s'il y a deux (ou plus) symboles '+', on gère l'erreur en gardant le premier (Transition de l'état 4)" << endl;
    cout << "Tous les autres cas d'erreur de syntaxes arrêtent l'analyse, en indiquant les symboles attendus et le symbole reçu" << endl;
+   cout << endl;
+   cout << "Exemple d'expression valide: (1+34)*123" << endl;
+   cout << "Exemple d'expression invalide (renvoie une erreur en arrêtant l'analyse): 1+*2" << endl;
+   cout << "Exemple d'expression invalide (renvoie une erreur en arrêtant l'analyse): 1+(2*3" << endl;
+   cout << "Exemple d'expression invalide (renvoie une erreur en arrêtant l'analyse): 1+2)" << endl;
+   cout << "Exemple d'expression invalide avec deux symboles '+' à la suite (Gestion de l'erreur en gardant le premier): 1++2" << endl;
+   cout << endl;
 
 
    string chaine;//("(1+34)*123)");
